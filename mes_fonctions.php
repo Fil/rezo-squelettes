72fc7efb1d0965c8439f0730971f4cc103d89315
Fil <fil@rezo.net>
2009-03-04 22:07:56 +0000

    on a la colonne principale, et presque la une

---
+++ mes_fonctions.php
@@ -0,0 +1,20 @@
+<?php
+
+// A partir d'un titre renvoyer titre, auteur, source
+function retitrage($titre, $quoi='titre') {
+  static $r = array();
+  if (!isset($r[$c = md5($titre)])) {
+    $r[$c] = array();
+    if (preg_match(',^(.*)\s+\(([^(]+)\)$,UimsS', trim($titre), $regs)) {
+      $titre = $regs[1];
+      $r[$c]['source'] = $regs[2];
+    }
+    if (preg_match('/^(.*),\s+(?:by|par)\s+(.+)$/UimsS', $titre, $regs)) {
+      $titre = $regs[1];
+      $r[$c]['auteurs'] = $regs[2];
+    }
+    $r[$c]['titre'] = $titre;
+  }
+
+  return $r[$c][$quoi];
+}
