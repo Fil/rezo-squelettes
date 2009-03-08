0b624f556c1d2a63cc9e92d3af611a2316589ef6
Fil <fil@rezo.net>
2009-03-08 15:54:01 +0000

    http://v3.rezo.net/ change de display quand on change de taille de fenetre

--- rezo.js
+++ rezo.js
@@ -1,11 +1,51 @@
 ;(function($) {
 
+	var memo = {};
+
+	$.fn.memove = function(dest) {
+		return $(this).each(function(){
+			var id = this.id;
+			if (!$(this).parent('.hold').length)
+				$(this).wrap('<div class="hold"><\/div>');
+			memo[id] = $(this).parent();
+			$(this).appendTo(dest);
+		});
+	};
+	$.restaure = function() {
+		$.each(memo, function(i) { $('#'+i).appendTo(memo[i]); });
+		memo = {};
+	};
+
+	var large = function() {
+		if ($('body').is('.large')) return;
+		$('body').addClass('large');
+		$('#citation')
+			.memove('#accueil');
+		$('#une,#depeches,#english')
+			.memove('#marge');
+		$('#marge').show();
+	};
+
+	var etroit = function() {
+		if (!$('body').is('.large')) return;
+		$('body').removeClass('large');
+		$('#marge').hide();
+		$.restaure();
+	}
+
+	var large_ou_etroit = function() {
+		if ($(window).width() > 1036)
+			large();
+		else
+			etroit();
+	};
+
 	// document.ready()
 	$(function(){
 
 		// cliquer un lien provoque un redirect pour les stats
 		// mais on remet le bon url dans le DOM
-		$('.hentry a[rel=bookmark]')
+		$('a[rel=bookmark]')
 		.click(function() {
 			var id  = $(this).parents('.hentry').attr('id');
 			if (id && id.match(/^a\d+/)) {
@@ -17,7 +57,7 @@
 		});
 
 		// le survol des liens affiche leur bloc introduction
-		$('.articles .hentry a[rel=bookmark]')
+		$('#contenu a[rel=bookmark]')
 		.hover(function(){
 			$(this).parents('.hentry').find('.introduction').show();
 		}, function() {
@@ -27,7 +67,7 @@
 		// Couper les dates repetitives
 		var vu = [];
 		var image = 1;
-		$('.articles .date').each(function(){
+		$('#contenu .date').each(function(){
 			var t = $(this).attr('class'); 
 			if (vu[t])
 				$(this).hide();
@@ -40,6 +80,11 @@
 		// Supprimer les abbr[title] des microformats
 		$('abbr.updated').attr('title', '');
 
+		// large ou etroit ?
+		$('#accueil').click(large);
+		$('#entete').click(etroit);
+		large_ou_etroit();
+		$(window).resize(large_ou_etroit);
 	});
 
 })(jQuery);
