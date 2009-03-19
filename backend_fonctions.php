5dda891b095126104c676f8af6fb35c5baa520e5
Fil <fil@rezo.net>
2009-03-19 16:32:38 +0000

    nommage: backend et hatom2rss

---
+++ backend_fonctions.php
@@ -0,0 +1,39 @@
+<?php
+
+function hatom2rss($uri) {
+
+	// lire la page hatom
+	$feed = recuperer_page(url_absolue($uri));
+
+	// choper les liens presents
+	preg_match_all(
+	',<div\s+class="hentry"\s+id="a(\d+)".*<abbr class="updated" title="(.*)">,UmsS',
+	$feed, $regs, PREG_SET_ORDER);
+
+	// trier par date et prendre les n plus recents
+	// et tous les plus recents que 3 jours
+	$items = $recents = array();
+	$troisjours = date('Y-m-d H:i:s', time()-3*24*3600);
+	foreach($regs as $reg) {
+		if ($reg[2] > $troisjours)
+			$recents[$reg[2].'-'.$reg[1]] = $reg[1];
+		else
+			$items[$reg[2].'-'.$reg[1]] = $reg[1];
+	}
+
+	krsort($recents);
+	$manquants = 5 - count($recents);
+	if ($manquants > 0) {
+		krsort($items);
+		$recents = array_merge($recents, array_slice($items,0,$manquants));
+	}
+
+	// recuperer les items correspondants
+	foreach($recents as $cle=>$id)
+		$recents[$cle] = microcache($id, 'inc-rss-item');
+
+	return join("\n\t", $recents);
+}
+
+
+?>
