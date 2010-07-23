6779d176b09ba6fa8941c42c7504afc1ebb1c0c1
Fil <fil@rezo.net>
2010-07-23 08:25:09 +0000

    reparer les backends

--- backend_fonctions.php
+++ backend_fonctions.php
@@ -2,19 +2,35 @@
 
 function hatom2rss($uri) {
 
-	// lire la page hatom
-	$feed = recuperer_page(url_absolue($uri));
-
 	// choper les liens presents
 	$type='a'; // a priori les articles
 	$max = 10;
-	if ($uri == 'tout') {
-		$type='[ab]'; // sauf pour backend/tout
-		$max = 100;
+
+	// sauf pour /backend/tout ou /feed/
+	switch($uri) {
+		case 'tout':
+		case '/themes/tout':
+			$type='[ab]'; 
+			$max = 100;
+			$uri = 'tout';
+			break;
+		case '/themes/%2Ffeed%2F':
+			$uri = 'tout';
+			$max = 20;
+			break;
+		case '/themes/bestof':
+			$uri = 'bestof';
+			$max = 20;
+			break;
 	}
+
+	// lire la page hatom
+	$feed = recuperer_page(url_absolue($uri));
+
 	preg_match_all(
 	',<div\s+class="hentry\b[^<>"]*"\s+id="'.$type.'(\d+)".*<abbr class="updated" title="(.*)">,UmsS',
 	$feed, $regs, PREG_SET_ORDER);
+
 
 	// trier par date et prendre les n plus recents
 	// et tous les plus recents que 3 jours
