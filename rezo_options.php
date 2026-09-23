cccca116dc6afaba45d727f60304ac1194d10ecb
brunob <bruno@eliaz.fr>
2026-09-23 15:57:24 +0200

    chore(sphinx): `$args` n'est pas défini, virer ce code dans lequel on ne passe jamais

--- rezo_options.php
+++ rezo_options.php
@@ -91,69 +91,6 @@ function sphinx_search($query, $conf = []) {
 	$ranker = SPH_RANK_EXPR;
 	$select = '';
 
-	for ($i = 0; $i < count($args); $i++) {
-		$arg = $args[$i];
-
-		if ($arg == '-h' || $arg == '--host') {
-			$host = $args[++$i];
-		} elseif ($arg == '-p' || $arg == '--port') {
-			$port = (int) $args[++$i];
-		} elseif ($arg == '-i' || $arg == '--index') {
-			$index = $args[++$i];
-		} elseif ($arg == '-s' || $arg == '--sortby') {
-			$sortby = $args[++$i];
-			$sortexpr = '';
-		} elseif ($arg == '-S' || $arg == '--sortexpr') {
-			$sortexpr = $args[++$i];
-			$sortby = '';
-		} elseif ($arg == '-a' || $arg == '--any') {
-			$mode = SPH_MATCH_ANY;
-		} elseif ($arg == '-b' || $arg == '--boolean') {
-			$mode = SPH_MATCH_BOOLEAN;
-		} elseif ($arg == '-e' || $arg == '--extended') {
-			$mode = SPH_MATCH_EXTENDED;
-		} elseif ($arg == '-e2') {
-			$mode = SPH_MATCH_EXTENDED2;
-		} elseif ($arg == '-ph' || $arg == '--phrase') {
-			$mode = SPH_MATCH_PHRASE;
-		} elseif ($arg == '-f' || $arg == '--filter') {
-			$filter = $args[++$i];
-		} elseif ($arg == '-v' || $arg == '--value') {
-			$filtervals[] = $args[++$i];
-		} elseif ($arg == '-g' || $arg == '--groupby') {
-			$groupby = $args[++$i];
-		} elseif ($arg == '-gs' || $arg == '--groupsort') {
-			$groupsort = $args[++$i];
-		} elseif ($arg == '-d' || $arg == '--distinct') {
-			$distinct = $args[++$i];
-		} elseif ($arg == '-l' || $arg == '--limit') {
-			$limit = (int) $args[++$i];
-		} elseif ($arg == '--select') {
-			$select = $args[++$i];
-		} elseif ($arg == '-fr' || $arg == '--filterrange') {
-			$cl->SetFilterRange($args[++$i], $args[++$i], $args[++$i]);
-		} elseif ($arg == '-r') {
-			$arg = strtolower($args[++$i]);
-			if ($arg == 'bm25') {
-				$ranker = SPH_RANK_BM25;
-			}
-			if ($arg == 'none') {
-				$ranker = SPH_RANK_NONE;
-			}
-			if ($arg == 'wordcount') {
-				$ranker = SPH_RANK_WORDCOUNT;
-			}
-			if ($arg == 'fieldmask') {
-				$ranker = SPH_RANK_FIELDMASK;
-			}
-			if ($arg == 'sph04') {
-				$ranker = SPH_RANK_SPH04;
-			}
-		} else {
-			$q .= $args[$i] . ' ';
-		}
-	}
-
 	foreach ($conf as $k => $v) {
 		${$k} = is_numeric($v) ? 0 + $v : $v;
 	}
