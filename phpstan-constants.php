feb632cfd576b9f1964cd95bd3cc225060f40e9e
brunob <bruno@eliaz.fr>
2026-09-24 12:57:49 +0200

    chore: phpstan level 1 & baseline

---
+++ phpstan-constants.php
@@ -0,0 +1,10 @@
+<?php
+
+define('_DIR_RACINE', '');
+define('_DIR_VAR', '');
+define('_DIR_TMP', '');
+define('_DIR_IMG', '');
+define('SPH_MATCH_ALL', 0);
+define('SPH_GROUPBY_ATTR', 4);
+define('SPH_SORT_EXTENDED', 4);
+define('SPH_SORT_EXPR', 5);
